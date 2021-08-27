ae604fd2f32e6811a0302664c978e000de3ca7a5
luanasantos.dsn.pack <luanasantos.dsn.pack@alterdata.com.br>
2021-08-27 11:03:52 -0300

    add sticky nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,30 @@
 const $menuBtn = document.querySelector('.btn-mobile-nav');
 const $header = document.querySelector('.header');
+const $sectionHero = document.querySelector('.section-hero')
 
+/* Nav menu */
 $menuBtn.addEventListener('click', () => {
     $header.classList.toggle('nav-open');
 });
+
+/* Sticky bar (wtf????????????)*/
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+
+        if(!ent.isIntersecting) {
+            document.body.classList.add('sticky');
+        }
+
+        if(ent.isIntersecting) {
+            document.body.classList.remove('sticky');
+        }
+}, 
+{
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px'
+});
+
+obs.observe($sectionHero);
+
